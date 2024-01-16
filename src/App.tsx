0acceaacf6d49b1f7a0f2db41d2f0532acdf1960
@mi477048 <mi477048@gmail.com>
2024-01-16 12:05:01 -0300

    update: clear btn added into the searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React, { useState } from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import Header from './components/header/Header';
-import MapComponent from './components/mapComponent/MapComponent';
+import HeroSection from './components/heroSection/HeroSection';
 import { PaperProvider } from 'react-native-paper';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import CustomSearchBar from './components/search/CustomSearchBar';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  const [region, setRegion] = useState([10.139444, 54.323334]);
-  const [searchQuery, setSearchQuery] = useState('');
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  console.log('Searching for:', searchQuery);
-
   return (
     <PaperProvider>
       <SafeAreaView style={{ flex: 1 }}>
         <Header />
-        <MapComponent region={region} setRegion={setRegion} />
+        <HeroSection />
       </SafeAreaView>
     </PaperProvider>
   );
@@ -30,18 +19,6 @@ const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
   },
 });
 
