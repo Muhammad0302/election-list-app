330406fef597e58e519e000103aeea7e88059544
@mi477048 <mi477048@gmail.com>
2024-01-16 00:01:56 -0300

    update: the scrolling issue fixed

--- src/components/mapComponent/MapComponent.tsx
+++ src/components/mapComponent/MapComponent.tsx
@@ -1,22 +1,47 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, FlatList } from 'react-native';
-
+import { data } from '../../../util/data';
 const MapComponent: React.FC = () => {
   const [searchText, setSearchText] = useState<string>('');
   const handleSearch = (text: string) => {
     setSearchText(text);
-    // Implement your search logic here using the searchText state
+
+    // const filteredData = data.filter((item) => {
+    //   // Convert each array item to a string and check if it includes the searchText
+    //   const itemAsString = item.join(''); // Convert the array to a string
+    //   return itemAsString.includes(text);
+    // });
+
+    // Now, filteredData contains only the items that completely match the searchText
+    // console.log('The filter data is:', filteredData);
   };
   interface Item {
     gharanaNo: string;
     salsalaNo: string;
     identityCardNo: string;
   }
-  const data = [
-    ['1', '22235643', '5.3466E'],
-    ['2', '22235644', '5.3466E'],
-    ['3', '22235645', '5.3466E'],
-  ];
+  // const data = [
+  //   ['1', '22235643', '5.3466E'],
+  //   ['2', '22235644', '5.3466E'],
+  //   ['3', '22235645', '5.3466E'],
+  //   ['4', '22235646', '5.3466E'],
+  //   ['5', '22235647', '5.3466E'],
+  //   ['6', '22235648', '5.3466E'],
+  //   ['7', '22235649', '5.3466E'],
+  //   ['8', '22235650', '5.3466E'],
+  //   ['9', '22235651', '5.3466E'],
+  //   ['10', '22235652', '5.3466E'],
+  //   ['11', '22235653', '5.3466E'],
+  //   ['12', '22235654', '5.3466E'],
+  //   ['13', '22235655', '5.3466E'],
+  //   ['14', '22235656', '5.3466E'],
+  //   ['15', '22235657', '5.3466E'],
+  //   ['16', '22235658', '5.3466E'],
+  //   ['17', '22235659', '5.3466E'],
+  //   ['18', '22235660', '5.3466E'],
+  //   ['19', '22235661', '5.3466E'],
+  //   ['20', '22235662', '5.3466E'],
+  // ];
   const renderItem = ({ item, index }: { item: string[]; index: number }) => (
     <View style={styles.listItem}>
       <Text>{item[0]}</Text>
@@ -36,7 +61,7 @@ const MapComponent: React.FC = () => {
     <View style={styles.container}>
       <TextInput
         style={styles.searchInput}
-        placeholder="Search for gharana no..."
+        placeholder="Search for Gharana no..."
         value={searchText}
         onChangeText={handleSearch}
       />
@@ -45,10 +70,10 @@ const MapComponent: React.FC = () => {
         data={data}
         renderItem={renderItem}
         keyExtractor={(item, index) => index.toString()}
-        // keyExtractor={(item) => item.gharanaNo}
         style={styles.flatList}
         contentContainerStyle={styles.contentContainer}
         keyboardShouldPersistTaps="handled"
+        stickyHeaderIndices={[0]}
       />
     </View>
   );
@@ -90,6 +115,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: 'gray',
     fontWeight: 'bold',
+    overflow: 'hidden', //
   },
   headerText: {
     fontWeight: 'bold',
