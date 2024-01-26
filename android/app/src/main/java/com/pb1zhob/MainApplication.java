818641ce769e56b57528b417f8eabc54f15569dc
@mi477048 <mi477048@gmail.com>
2024-01-26 15:18:45 -0300

    update: changes added to pb1

--- android/app/src/main/java/com/pb1zhob/MainApplication.java
+++ android/app/src/main/java/com/pb1zhob/MainApplication.java
@@ -1,4 +1,4 @@
-package com.pb2zhob;
+package com.pb1zhob;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/PB1ZhobTests/PB1ZhobTests.m
+++ ios/PB1ZhobTests/PB1ZhobTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface PB2ZhobTests : XCTestCase
+@interface PB1ZhobTests : XCTestCase
 
 @end
 
-@implementation PB2ZhobTests
+@implementation PB1ZhobTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
